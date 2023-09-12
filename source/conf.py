617533c1a1e9fcd636a91f04c75790274e8eff08
Raj <vignesh.raj@gainwelltechnologies.com>
2023-09-12 15:05:55 +0530

    test

--- source/conf.py
+++ source/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(1, os.path.abspath('../11610923'))
-sys.path.insert(2, os.path.abspath('../acuity_de_batchingmonitor'))
+# sys.path.insert(1, os.path.abspath('../11610923'))
+# sys.path.insert(2, os.path.abspath('../acuity_de_batchingmonitor'))
 
 # -- Project information -----------------------------------------------------
 
